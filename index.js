035a9daa78507e89b59a478d9271c60b60ff1de0
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2014-01-07 02:09:13 -0800

    Update for Tennu-Modules 2.0.0

--- index.js
+++ index.js
@@ -6,72 +6,62 @@
  */
 
 
-var dados = require("dados");
-var Hrandom = require('hrandom');
+const dados = require('dados');
+const Hrandom = require('hrandom');
 
-module.exports = function TennuLuckModule (tennu) {
-    var Random = new Hrandom(Date.now());
+module.exports = {
+    init: function TennuLuckModule (tennu) {
+        const Random = new Hrandom(Date.now());
 
-    function roll (command) {
-        var request = command.args.join("");
-        var result = dados.roll(request);
+        function roll (command) {
+            const request = command.args.join('');
+            const result = dados.roll(request);
 
-        if (command.isSubcommand) {
-            return result;
+            if(!isNaN(result)) {
+                return 'Rolling ' + request + ' | Result: ' + result;
+            } else {
+                return 'Failed to roll ' + request + '.';
+            }
         }
 
-        if(!isNaN(result)) {
-            return "Rolling " + request + " | Result: " + result;
-        } else {
-            return "Failed to roll " + request + ".";
+        function which (command) {
+            const chosen = Random.nextElement(command.args);
+
+            return 'Choosing ' + chosen + '.');
         }
+
+        function sample (command) {
+            const args = command.args.slice();
+            const n = Math.floor(+args.shift());
+
+            if (n === 0) {
+                return 'Sampling nothing.';
+            }
+
+            if (n < 0) {
+                return 'Error: Cannot sample negative options.';
+            }
+
+            if (n > args.length) {
+                return 'Not enough options to sample.';
+            }
+
+            const sampled = Random.nextElements(args, n);
+
+            return 'Sampled [' + sampled.join(', ') + ']';
+        }
+
+        return {
+            exports : {
+                'Random' : Random
+            },
+
+            handlers : {
+                '!roll' : roll,
+                '!choose !which' : which,
+                '!sample' : sample
+            }
+        };
+
     }
-
-    function which (command) {
-        var chosen =  Random.nextElement(command.args);
-
-        if (command.isSubcommand) {
-            return chosen;
-        }
-
-        return 'Choosing ' + chosen + '.');
-    }
-
-    function sample (command) {
-        var args = command.args.slice();
-        var n = Math.floor(+args.shift());
-
-        if (n === 0) {
-            return "Sampling nothing.";
-        }
-
-        if (n < 0) {
-            return "Error: Cannot sample negative options.";
-        }
-
-        if (n > args.length) {
-            return "Not enough options to sample.";
-        }
-
-        var sampled = Random.nextElements(args, n);
-
-        if (command.isSubcommand) {
-            return sampled;
-        }
-
-        return 'Sampled [' + sampled.join(', ') + ']';
-    }
-
-    return {
-        exports : {
-            "Random" : Random
-        },
-
-        handlers : {
-            "!roll" : roll,
-            "!choose !which" : which,
-            "!sample" : sample
-        }
-    };
-
 };
